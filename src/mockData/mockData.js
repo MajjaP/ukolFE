57dfa18bdacddcf912d738d0676675c11c2a7bf4
“MajjaP” <marie.prouzova@gmail.com>
2023-12-12 15:21:49 +0100

    komentar

---
+++ src/mockData/mockData.js
@@ -0,0 +1,24 @@
+const users = [
+  {
+    id: "7557-2325-458-0000",
+    name: "Marie Prouzova Owner",
+  },
+  {
+    id: "7557-2325-458-0001",
+    name: "Jiri Prouza Member",
+  },
+  {
+    id: "7557-2325-458-0002",
+    name: "Anna Prouzova Member",
+  },
+  {
+    id: "7557-2325-458-0003",
+    name: "Lucie Prouzova Other",
+  },
+  {
+    id: "7557-2325-458-0004",
+    name: "Justyna Prouzova Other",
+  },
+];
+
+export { users };
